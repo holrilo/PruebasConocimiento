593d0e116abccf7591463acd3c1440eb0b928299
holrilo <senky98@gmail.com>
2021-10-16 18:15:47 -0500

    Se crea las Migraciones

---
+++ database/migrations/2021_10_16_230742_create_tareas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTareasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_tariea');
+            $table->string('descripcion_tarea');
+            $table->dateTime('fecha_creacion',$precision =0);
+            $table->string('estado_tarea');
+            $table->dateTime('fecha_vencimiento',$precision=0);
+            $table->string('usuario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tareas');
+    }
+}
